204fc426c52c66bd95a39dbb8c16d3efe6e47088
dontliem1 <dontliem1@yandex-team.ru>
2022-10-29 05:28:48 +0500

    2018

---
+++ plugins/folga/mlp/models/Project.php
@@ -0,0 +1,30 @@
+<?php namespace Folga\Mlp\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Project extends Model
+{
+    use \Winter\Storm\Database\Traits\Validation;
+
+    /*
+     * Disable timestamps by default.
+     * Remove this line if timestamps are defined in the database table.
+     */
+    public $timestamps = false;
+
+    protected $jsonable = ['gallery', 'team'];
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'folga_mlp_projects';
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+    ];
+}
---
+++ plugins/folga/mlp/updates/builder_table_create_folga_mlp_projects.php
@@ -0,0 +1,30 @@
+<?php namespace Folga\Mlp\Updates;
+
+use Schema;
+use Winter\Storm\Database\Updates\Migration;
+
+class BuilderTableCreateFolgaMlpProjects extends Migration
+{
+    public function up()
+    {
+        Schema::create('folga_mlp_projects', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('title');
+            $table->smallInteger('year')->unsigned();
+            $table->boolean('active');
+            $table->string('slug');
+            $table->string('link')->nullable();
+            $table->text('gallery')->nullable();
+            $table->text('content')->nullable();
+            $table->text('team')->nullable();
+            $table->integer('sort_order')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('folga_mlp_projects');
+    }
+}
